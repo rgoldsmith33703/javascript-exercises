a8a6c7336025ddf414a36d574c62020c583f62bb
rgoldsmith33703 <rgolds33703@outlook.com>
2022-08-13 13:56:07 -0400

    complete exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,6 @@
-const removeFromArray = function() {
-
-};
+const removeFromArray = function(arr, ...remove) {
+    return arr.filter(e => !remove.includes(e))
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
