bf2f4f6fba8b1678fcb11303cdb8b3e2c2bfae32
rgoldsmith33703 <rgolds33703@outlook.com>
2022-08-13 14:49:30 -0400

    complete exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,24 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    finalSum = 0;
 
+    if (!isPositiveNumber(num1) || !isPositiveNumber(num2)) {
+        return 'ERROR'
+    } else if (num1 > num2) {
+        [num1, num2] = [num2, num1]
+    } 
+    
+    for (i=num1; i<=num2; i++) {
+            finalSum += i
+        }
+    
+    return finalSum
 };
+
+function isPositiveNumber(num) {
+    if ((num > 0) && (Number.isInteger(num))) {
+        return true
+    }
+}
 
 // Do not edit below this line
 module.exports = sumAll;
