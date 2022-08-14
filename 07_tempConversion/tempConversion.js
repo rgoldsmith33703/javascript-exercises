343c63a0e0632ca97a43cfd4a887650b1e84e926
rgoldsmith33703 <rgolds33703@outlook.com>
2022-08-14 01:34:28 -0400

    complete exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,16 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  let newTemp = (temp - 32) * (5/9);
+  return roundToOne(newTemp);
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  let newTemp = temp * (9/5) + 32;
+  return roundToOne(newTemp);
 };
+
+function roundToOne(num) {
+  return Math.round(num * 10) / 10
+}
 
 // Do not edit below this line
 module.exports = {
